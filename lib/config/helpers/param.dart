9f6121d8e3804698baeef31d3b85cad7729f97b7
German Vianna <gvianna@pqs.com.ar>
2023-06-03 18:38:53 -0300

    reverse change

--- lib/config/helpers/param.dart
+++ lib/config/helpers/param.dart
@@ -1,7 +1,7 @@
 import 'package:path_provider/path_provider.dart';
 
 class Param {
-  static const urlServer = "http://192.168.0.107:9292/speak-app";
+  static const urlServer = "http://172.17.208.1:9292/speak-app/";
   static const postTranscription = "/speech-recognition/transcription";
   static const modelWhisper = "whisper-1";
 
