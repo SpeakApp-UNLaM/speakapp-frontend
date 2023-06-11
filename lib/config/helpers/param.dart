a39c8ebd9e9689038814ec7e13c36d5bd7e7711d
nico.ing.work@outlook.com <nico.ing.work@outlook.com>
2023-06-10 21:36:03 -0300

    IP local fixed
    investigación + ajuste IPV4 para que sea equivalente a "localhost"

--- lib/config/helpers/param.dart
+++ lib/config/helpers/param.dart
@@ -1,5 +1,6 @@
 class Param {
-  static const urlServer = "http://172.17.208.1:9292/speak-app/";
+  //10.0.2.2 IP especial para emuladores, que mapea la IP del HOST el cual está ejecutando (equivalente a LOCALHOST)
+  static const urlServer = "http://10.0.2.2:9292/speak-app/";
   static const postTranscription = "/speech-recognition/transcription";
   static const getCareers = "/careers";
   static const getExercises = "/exercises";
