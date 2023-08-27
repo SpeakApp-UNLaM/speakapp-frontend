39ad37a9ed160d525b5f09144439116cc4474a03
German Vianna <gvianna@pqs.com.ar>
2023-08-27 20:12:07 -0300

    arreglo para que no explote el home

--- lib/presentations/screens/home_screen.dart
+++ lib/presentations/screens/home_screen.dart
@@ -52,13 +52,12 @@ class HomeScreen extends StatelessWidget {
           Padding(
               padding: const EdgeInsets.only(right: 20.0),
               child: GestureDetector(
-                onTap: () {},
-                child: const CircleAvatar(
-                  //TODO GET IMAGE FROM USER
-                  backgroundImage: NetworkImage(
-                      'https://img.freepik.com/foto-gratis/nino-sonriente-aislado-rosa_23-2148984798.jpg?w=1380&t=st=1689548961~exp=1689549561~hmac=ca7f09fd97ffda2f39c4e258f2ae1c44b69bab0c6423cd0ec106c492a469caf2'),
-                ),
-              )),
+                  onTap: () {},
+                  child: const CircleAvatar(
+                    //TODO GET IMAGE FROM USER
+                    backgroundImage:
+                        AssetImage('assets/branding/Logo_SpeakApp.png'),
+                  ))),
         ],
         bottom: PreferredSize(
             preferredSize: const Size.fromHeight(1.0),
